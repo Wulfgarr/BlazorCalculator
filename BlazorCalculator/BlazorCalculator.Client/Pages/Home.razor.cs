6e74ba507216ac0c05072b1fbe08be20c2e9a567
mhanusiak <maciej.hanusiak7@gmail.com>
2026-06-27 23:01:45 +0200

    feat(logic): Bind calculator buttons with methods

--- BlazorCalculator/BlazorCalculator.Client/Pages/Home.razor.cs
+++ BlazorCalculator/BlazorCalculator.Client/Pages/Home.razor.cs
@@ -2,5 +2,26 @@
 {
     public partial class Home
     {
+        private string _displayValue = "0";
+
+        private void AppendInput(string input)
+        {
+
+        }
+
+        private void OnOperator(string op)
+        {
+
+        }
+
+        private void OnClear()
+        {
+
+        }
+
+        private void OnEquals()
+        {
+
+        }
     }
 }
